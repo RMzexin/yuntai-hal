b5a76714d120162ba5d56b51c3fe166933e2bb8f
RMzexin <1045699218@qq.com>
2018-12-31 15:37:14 +0800

    Revert "RMzexin"
    This reverts commit 3938bdd05552605282504b999625cbf7bdf5e451.

--- Src/main.c
+++ Src/main.c
@@ -136,6 +136,10 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+		Gimbal_RC_Mode();
+		SHORT();
+		PID_calculate_position_self();
+		PID_calculate_chassis_self();
   }
   /* USER CODE END 3 */
 }
@@ -199,10 +203,6 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
   /* USER CODE BEGIN Callback 0 */
 	if (htim->Instance == TIM3){
-	  Gimbal_RC_Mode();
-		SHORT();
-		PID_calculate_position_self();
-		PID_calculate_chassis_self();
 }
   /* USER CODE END Callback 0 */
   if (htim->Instance == TIM6) {
